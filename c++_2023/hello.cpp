7bcdd36547459033ee7b68a3fabd352bfc221b8f
yorlysoro <yorlysoro@gmail.com>
2023-05-18 21:27:40 -0400

    comments first program

---
+++ c++_2023/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// One Line Comment
+/*
+    Multiple Lines
+    Comments
+*/
+int main(){
+    printf("Hello World\n");
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
---
+++ c++_2023/vars.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    int one = 1;
+    float two = 2.22;
+    double three = 3.3333;
+    char four = 'A';
+    //String five = 'Hello';
+    std::cout << one << std::endl;
+    std::cout << two << std::endl;
+    std::cout << three << std::endl;
+    std::cout << four << std::endl;
+
+
+    return 0;
+}
